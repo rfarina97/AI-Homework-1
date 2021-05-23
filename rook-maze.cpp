5e3c6a4e6dbdd59e51b9657a735a12aab011a4a1
rfarina97 <rodolfo.farina.2015@gmail.com>
2021-05-23 15:50:07 -0400

    final commit

--- rook-maze.cpp
+++ rook-maze.cpp
@@ -51,24 +51,25 @@ int BFS(int* mat, int row, int col, int sX, int sY, int eX, int eY){
 	//add root to queue
 	pair<int,int> root(sX, sY);
 	pending.push(root);
-	cout << "Root is " << root.first << " " << root.second << endl;
+	//cout << "Root is " << root.first << " " << root.second << endl;
 
 
 	pair<int,int> aux;
 	pair<int,int> res;
 	int checkVal = -1;
 	int posUp, posLeft, posRight, posDown;
+	bool solved = false;
 
 	//check possible locations to be explored
 	while (!pending.empty()){
 		//dequeue front and save it as aux
 		aux = pending.front();
 		pending.pop();
-		cout << "Current position: " << aux.first << " " << aux.second << endl;
+		//cout << "Current position: " << aux.first << " " << aux.second << endl;
 
 		//check if aux is goal
 		if (aux.first == eX && aux.second == eY){
-			cout << "GOAL REACHED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
+			/*cout << "GOAL REACHED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
 			cout << "Steps: " << steps << endl;
 			for (int i = 0; i < row; ++i)
 			{
@@ -83,24 +84,25 @@ int BFS(int* mat, int row, int col, int sX, int sY, int eX, int eY){
 					
 				}
 				cout << endl;
-			}
+			}*/
+			solved = true;
 			break;
 			//return steps;
 		}
 
 		checkVal = mat[aux.first * col + aux.second]; //current value to be checked
-		cout << "Check values " << checkVal << endl;
+		//cout << "Check values " << checkVal << endl;
 
 		//calculate 4 possible positions 
 		posUp = aux.first - checkVal;
 		posDown = aux.first + checkVal;
 		posLeft = aux.second - checkVal;
-		posRight = aux.second + checkVal;
+		posRight = aux.second + checkVal;/*
 		cout << "I: " << aux.first << " " << aux.second << endl;
 		cout << "Up: " << posUp << " " << aux.second << endl;
 		cout << "Down: " << posDown << " " << aux.second << endl;
 		cout << "Left: " << aux.first << " " << posLeft << endl;
-		cout << "Right: " << aux.first << " " << posRight << endl;
+		cout << "Right: " << aux.first << " " << posRight << endl;*/
 
 
 
@@ -111,10 +113,10 @@ int BFS(int* mat, int row, int col, int sX, int sY, int eX, int eY){
 			if (visited[posUp][aux.second] == false){
 				res = make_pair(posUp, aux.second);
 				pending.push(res);
-				cerr << "Pushing Up: " << posUp << " " << aux.second << endl;
+				//cerr << "Pushing Up: " << posUp << " " << aux.second << endl;
 				visited[aux.first][aux.second] = true;
 
-				cerr << "Attempting to put pair in " << posUp << " " << aux.second << endl;
+				//cerr << "Attempting to put pair in " << posUp << " " << aux.second << endl;
 				parent[posUp][aux.second] = make_pair(aux.first, aux.second);
 			}
 		}
@@ -122,10 +124,10 @@ int BFS(int* mat, int row, int col, int sX, int sY, int eX, int eY){
 			if (visited[posDown][aux.second] == false){
 				res = make_pair(posDown, aux.second);
 				pending.push(res);
-				cerr << "Pushing Down: " << posDown << " " << aux.second << endl;
+				//cerr << "Pushing Down: " << posDown << " " << aux.second << endl;
 				visited[aux.first][aux.second] = true;
 
-				cerr << "Attempting to put pair in " << posDown << " " << aux.second << endl;
+				//cerr << "Attempting to put pair in " << posDown << " " << aux.second << endl;
 				parent[posDown][aux.second] = make_pair(aux.first, aux.second);
 			}			
 		}
@@ -133,10 +135,10 @@ int BFS(int* mat, int row, int col, int sX, int sY, int eX, int eY){
 			if (visited[aux.first][posLeft] == false){
 				res = make_pair(aux.first, posLeft);
 				pending.push(res);
-				cerr << "Pushing Left: " << aux.first << " " << posLeft << endl;
+				//cerr << "Pushing Left: " << aux.first << " " << posLeft << endl;
 				visited[aux.first][aux.second] = true;
 
-				cerr << "Attempting to put pair in " << aux.first << " " << posLeft << endl;
+				//cerr << "Attempting to put pair in " << aux.first << " " << posLeft << endl;
 				parent[aux.first][posLeft] = make_pair(aux.first, aux.second);
 			}			
 		}
@@ -144,21 +146,26 @@ int BFS(int* mat, int row, int col, int sX, int sY, int eX, int eY){
 			if (visited[aux.first][posRight] == false){
 				res = make_pair(aux.first, posRight);
 				pending.push(res);
-				cerr << "Pushing Right: " << aux.first << " " << posRight << endl;
+				//cerr << "Pushing Right: " << aux.first << " " << posRight << endl;
 				visited[aux.first][aux.second] = true;
 
-				cerr << "Attempting to put pair in " << aux.first << " " << posRight << endl;
+				//cerr << "Attempting to put pair in " << aux.first << " " << posRight << endl;
 				parent[aux.first][posRight] = make_pair(aux.first, aux.second);
 			}			
 		}
 
 		steps++;
-		cout << "-----------------------\n";
+		//cout << "-----------------------\n";
 
 	}
 
+	if (solved == false){
+		cout << "No hay solucion\n";
+		return -1;
+	}
+
 	pair<int,int> temp = make_pair(eX, eY);
-	cout << eX << " " << eY << endl;
+	//cout << eX << " " << eY << endl;
 	vector<pair<int,int>> road;
 	//print path
 	while(1){
@@ -173,11 +180,15 @@ int BFS(int* mat, int row, int col, int sX, int sY, int eX, int eY){
 		}
 	}
 
-	cout << "ROAD TAKEN: " << endl;
-	for (int i = road.size()-1 ; i >= 0; --i)
+	//prints road taken
+	int count = 0;
+	//cout << "ROAD TAKEN: " << endl;
+	for (int i = road.size()-2 ; i >= 0; --i)
 	{
-		cout<<road[i].first << " " << road[i].second << endl;
+		//cout<<road[i].first << " " << road[i].second << endl;
+		count++;
 	}
+	cout << count << endl;
 	//mat[5] = 999;
 	return -1;
 }
@@ -186,41 +197,48 @@ int BFS(int* mat, int row, int col, int sX, int sY, int eX, int eY){
 int main()
 {
 
-	//n rows
-	//m columns
-	//start position
-	int m, n;
-	int startX, startY;
-	int endX, endY;
+	
 
-	//assume start and end are always within the bounds of the matrix
-	cin >> n >> m;
-	cin >> startX >> startY;
-	cin >> endX >> endY;
+	while(1){
+		//n rows
+		//m columns
+		//start position
+		int m, n;
+		int startX, startY;
+		int endX, endY;
 
-	//matrix
-	int mat[n*m];
+		//assume start and end are always within the bounds of the matrix
+		cin >> n >> m;
+		cin >> startX >> startY;
+		cin >> endX >> endY;
 
-	//read matrix input
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			cin >> mat[i*m + j];
+		if (n == 0){
+			break;
 		}
-	}
+		//matrix
+		int mat[n*m];
 
-	//print matrix for debug
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
+		//read matrix input
+		for (int i = 0; i < n; ++i)
 		{
-			cout << mat[i*m + j] << " ";
+			for (int j = 0; j < m; ++j)
+			{
+				cin >> mat[i*m + j];
+			}
 		}
-		cout << endl;
-	}
 
-	BFS(mat, n, m, startX, startY, endX, endY);
+		//print matrix for debug
+		/*for (int i = 0; i < n; ++i)
+		{
+			for (int j = 0; j < m; ++j)
+			{
+				cout << mat[i*m + j] << " ";
+			}
+			cout << endl;
+		}*/
+
+		BFS(mat, n, m, startX, startY, endX, endY);
+	}
 
 	/*for (int i = 0; i < n; ++i)
 	{
